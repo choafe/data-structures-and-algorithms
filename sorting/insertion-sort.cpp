2c8e5880d2fb48ee747e9f396a6c1e2dc0b4e1a6
choafe <cryn.hoang@gmail.com>
2023-05-18 13:33:23 -0500

    Completed modified variation of selection sort

--- sorting/insertion-sort.cpp
+++ sorting/insertion-sort.cpp
@@ -3,10 +3,6 @@
 Insertion sorting algorithm
 Compare first two objects and swap them if the left is larger than the right
 The right sub array will always be sorted (or you can proceed with a left sub array instead)
-
-Example
-Input: 5 1 6 2 4 3
-Output: 1 2 3 4 5 6
 */
 
 #include <iostream>
@@ -21,23 +17,24 @@ void recurInsertion()
 
 void iterInsertion(vector<int>& arr) 
 {
+
 }
 
 void printSorted(vector<int>& arr) 
 {
     for(int i = 0; i < arr.size(); i++) 
     {
-        cout << arr[i] << endl;
+        cout << arr[i] << ' ';
     }
+    cout << endl;
 }
 
+// Driver code for testing and visualizing implementation of code
 int main(void) 
 {
     vector<int> arr1 = {5, 1, 6, 2, 4, 3};
-    vector<int> arr2 = {5, 1, 6, 2, 4, 3};
-
+    
     printSorted(arr1);
-    printSorted(arr2);
  
     return 0;
 }
--- sorting/selection-sort.cpp
+++ sorting/selection-sort.cpp
@@ -10,3 +10,60 @@ Switch between the smallest and largest value for sorting (faster)
 
 #include <iostream>
 #include <vector>
+
+using namespace std;
+ 
+void printSorted(vector<int>& arr) 
+{
+    for(int i = 0; i < arr.size(); i++) 
+    {
+        cout << arr[i] << ' ';
+    }
+    cout << endl;
+}
+
+// 5-18 Squares input vector and sorts by increasing order
+vector<int> sortedSquares(vector<int>& nums) 
+{
+    for(int i = 0; i < nums.size(); i++) 
+    {
+        nums[i] = nums[i]*nums[i];
+    }
+
+    // Two pointer system for selection sort
+    int i = 0, j = nums.size()-1;
+    while(i < j) 
+    {     
+        if(nums[i] > nums[j]) 
+        {
+            int temp = nums[j];
+            nums[j] = nums[i];
+            nums[i] = temp;
+        }
+
+        if(i == j-1)
+        {
+            i = 0;
+            j--;
+            printSorted(nums);
+        } 
+        else 
+        {
+            i++;
+        }
+    }
+    return nums;
+}
+
+
+// Driver code for testing and visualizing implementation of code
+int main(void) 
+{
+    vector<int> arr = {64, 25, 12, 22, 11};
+
+    sortedSquares(arr);
+    cout << "Sorted Array: " << endl;
+    printSorted(arr);
+ 
+    return 0;
+}
